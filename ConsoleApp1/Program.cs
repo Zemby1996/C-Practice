42c10b29a454164fa464f2032f4ae331f9e00e1b
Zemby1996 <158062891+Zemby1996@users.noreply.github.com>
2024-11-11 21:00:31 +0100

    11-11-2024

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+
+        static bool TryParseToNegativeInt(string input, out int result)
+        {
+
+            if(int.TryParse(input, out result))
+            {
+                if(result < 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                result = default;
+                return false;
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            int inputValue;
+
+            while(!TryParseToNegativeInt(Console.ReadLine(), out inputValue))
+            {
+                Console.WriteLine("Insert negative number");
+            }
+
+            Console.WriteLine($"Inserted negative number = {inputValue}");
+
+        }
+    }
+}
---
+++ TemperatureConverter/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace TemperatureConverter
+{
+
+    public class TemperatureConverter
+    {
+        public void ConvertCelsiusToFahrenheit(double celsius, out double fahrenheit)
+        {
+            fahrenheit = celsius * 1.8 + 32;
+        }
+
+        public void ConvertFahrenheitToCelsius(double fahrenheit, ref double celsius)
+        {
+            
+            celsius = (fahrenheit - 32) / 1.8;
+
+        }
+    }
+
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+            TemperatureConverter converter = new TemperatureConverter();
+
+            double tempC = 25;
+            double tempF;
+
+            converter.ConvertCelsiusToFahrenheit(tempC, out tempF);
+
+            Console.WriteLine($"{tempC} Celsujsza równa się {tempF} Farenheita");
+
+            double tempFa = 77;
+            double tempCe = 0;
+
+            converter.ConvertFahrenheitToCelsius(tempFa, ref tempCe);
+
+            Console.WriteLine($"{tempFa} Farenheita równa się {tempCe} Celsujsza");
+
+        }
+    }
+}
